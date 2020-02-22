2831f6bac00ca1b6e133f54e2c9c8de34d6809b5
Vladimir N.Dyakov <v@4230395.ru>
2020-02-22 23:54:41 +0300

    Some changes

--- install.sh
+++ install.sh
@@ -7,49 +7,156 @@
 apt update && apt upgrade -y
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 apt update
-apt install -y docker-ce mc ifstat atop htop mtr git tcpdump rsync ca-certificates vnstat dnsutils smartmontools libwww-perl fail2ban strace
+apt install -y \
+    zip \
+    curl \
+    docker-ce \
+    mc \
+    ifstat \
+    atop \
+    htop \
+    mtr \
+    git \
+    tcpdump \
+    rsync \
+    ca-certificates \
+    vnstat \
+    dnsutils \
+    smartmontools \
+    libwww-perl \
+    fail2ban \
+    strace \
+    sysfsutils \
+    ntpdate \
+    lsof \
+    net-tools \
+    bind-utils \
+    telnet \
+    traceroute \
+    vim \
+    jq \
+    tmux \
+    screen \
+    logrotate \
+    iotop \
+    bash-completion \
+    bash-completion-extras \
+    bc \
+    sshpass
+
 curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # some basic sysctl tweaks
-echo "kernel.shmmax = 1073741824" >> /etc/sysctl.d/99-tweaks.conf
-echo "kernel.shmmax = 1073741824" >> /etc/sysctl.d/99-tweaks.conf
-echo "kernel.pid_max = 2097152" >> /etc/sysctl.d/99-tweaks.conf
-echo "kernel.sched_migration_cost_ns = 5000000" >> /etc/sysctl.d/99-tweaks.conf
-echo "fs.file-max = 2097152" > /etc/sysctl.d/99-tweaks.conf
-echo "net.core.somaxconn = 65535" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.core.netdev_max_backlog = 65535" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.core.rmem_max = 25165824" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.core.rmem_default = 25165824" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.core.wmem_max = 25165824" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.core.wmem_default = 65536" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.core.optmem_max = 25165824" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.ip_local_port_range = 2000 65535" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_tw_reuse = 1" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_max_syn_backlog = 40000" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_rmem = 4096 25165824 25165824" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_wmem = 4096 65536 25165824" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_fin_timeout = 25" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_keepalive_time=60" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_keepalive_probes=3" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_keepalive_intvl=15" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_sack = 0" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_mtu_probing = 0" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.ipv4.tcp_rfc1337 = 1" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.netfilter.nf_conntrack_max = 1548576" >> /etc/sysctl.d/99-tweaks.conf
-echo "net.nf_conntrack_max = 1548576" >> /etc/sysctl.d/99-tweaks.conf
-echo "vm.swappiness = 5" >> /etc/sysctl.d/99-tweaks.conf
-echo "vm.overcommit_memory = 1" >> /etc/sysctl.d/99-tweaks.conf
+>/etc/sysctl.d/99-tweaks.conf << EOF
+kernel.shmmax = 1073741824
+kernel.shmmax = 1073741824
+kernel.pid_max = 2097152
+kernel.sched_migration_cost_ns = 5000000
+fs.file-max = 2097152
+fs.inotify.max_user_instances = 8192
+fs.inotify.max_user_watches = 524288
+net.core.somaxconn = 65535
+net.core.netdev_max_backlog = 65535
+net.core.rmem_max = 25165824
+net.core.rmem_default = 25165824
+net.core.wmem_max = 25165824
+net.core.wmem_default = 65536
+net.core.optmem_max = 25165824
+net.ipv4.ip_local_port_range = 2000 65535
+net.ipv4.tcp_syncookies = 0
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_max_syn_backlog = 40000
+net.ipv4.tcp_rmem = 4096 25165824 25165824
+net.ipv4.tcp_wmem = 4096 65536 25165824
+net.ipv4.tcp_fin_timeout = 25
+net.ipv4.tcp_keepalive_time = 60
+net.ipv4.tcp_keepalive_probes = 3
+net.ipv4.tcp_keepalive_intvl = 15
+net.ipv4.tcp_sack = 0
+net.ipv4.tcp_mtu_probing = 0
+net.ipv4.tcp_rfc1337 = 1
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_orphan_retries = 1
+net.ipv4.netfilter.ip_conntrack_max = 1548576
+net.netfilter.nf_conntrack_max = 1548576
+net.nf_conntrack_max = 1548576
+vm.swappiness = 1
+vm.overcommit_memory = 1
+EOF
+
 sysctl -p /etc/sysctl.d/99-tweaks.conf
 
 # systemd tweaks
-sed -i 's/#DefaultLimitNOFILE=/DefaultLimitNOFILE=2097152/g' /etc/systemd/system.conf
-sed -i "s/#DefaultLimitMEMLOCK=/DefaultLimitMEMLOCK=infinity/g" /etc/systemd/system.conf
-sed -i "s/#DefaultLimitAS=/DefaultLimitAS=infinity/g" /etc/systemd/system.conf
-sed -i "s/#DefaultLimitRSS=/DefaultLimitRSS=infinity/g" /etc/systemd/system.conf
-sed -i "s/#DefaultLimitCORE=/DefaultLimitCORE=infinity/g" /etc/systemd/system.conf
+>/etc/systemd/system.conf << EOF
+[Manager]
+DefaultLimitNOFILE=2097152
+DefaultLimitMEMLOCK=infinity
+DefaultLimitAS=infinity
+DefaultLimitRSS=infinity
+DefaultLimitCORE=infinity
+DefaultLimitNPROC=320000
+EOF
+
+>/etc/systemd/user.conf << EOF
+[Manager]
+DefaultLimitNOFILE=2097152
+DefaultLimitMEMLOCK=infinity
+DefaultLimitAS=infinity
+DefaultLimitRSS=infinity
+DefaultLimitCORE=infinity
+DefaultLimitNPROC=320000
+EOF
+
 systemctl daemon-reload
+
+# pam tweaks
+>/etc/security/limits.conf << EOF
+# /etc/security/limits.conf
+* - memlock unlimited
+* - nofile 2097152
+* - nproc 32768
+* - as unlimited
+* - stack unlimited
+root - memlock unlimited
+root - nofile 2097152
+root - nproc 32768
+root - as unlimited
+root - stack unlimited
+# End of file
+
+EOF
+
+echo "session required pam_limits.so" >> "/etc/pam.d/common-session"
+
+### SSH config
+>/etc/ssh/sshd_config << EOF
+Port 22
+Protocol 2
+PermitUserEnvironment yes
+PermitRootLogin without-password
+AllowUsers root update backuppc secure
+X11Forwarding no
+ChallengeResponseAuthentication no
+PubkeyAuthentication yes
+RSAAuthentication yes
+GSSAPIAuthentication no
+ChallengeResponseAuthentication no
+UseDNS no
+PasswordAuthentication no
+PermitEmptyPasswords no
+UsePAM yes
+MaxAuthTries 5
+PrintMotd no
+AcceptEnv LANG LC_*
+Subsystem	sftp	/usr/lib/openssh/sftp-server
+EOF
+service ssh restart
+
+### NtpDate sync
+apt remove ntp -y
+echo "0 * * * * root /usr/sbin/ntpdate 1.ru.pool.ntp.org 1>/dev/null 2>&1" > /etc/cron.d/ntpdate
+
